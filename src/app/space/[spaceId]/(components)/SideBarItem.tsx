f587ea8b48f21d281a420c5c16cf64d1310f559b
Nick Fanelli <nickfanelli5@gmail.com>
2024-06-04 10:01:17 -0400

    feat: make sidebaritems component a server component

--- src/app/space/[spaceId]/(components)/SideBarItem.tsx
+++ src/app/space/[spaceId]/(components)/SideBarItem.tsx
@@ -1,28 +1,32 @@
+"use client";
+
 import { IconDefinition } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import Link from "next/link"
+import { usePathname } from "next/navigation"
 
 type Props = {
     
     displayName: string
     route: string
     icon?: IconDefinition
-    activePath: string
 
 }
 
 const SideBarItem = (props: Props) => {
 
+    const pathname = usePathname();
+
     return (
-        <div className={`w-11/12 pl-5 rounded-md py-2 cursor-pointer hover:bg-base-300 ${props.activePath.startsWith(props.route) ? "bg-base-300" : ""}`}>
-            <Link href={props.route} className="flex justify-start items-center w-full h-full">
+        <Link className={`w-11/12 pl-5 rounded-md py-2 cursor-pointer hover:bg-base-300 ${pathname.startsWith(props.route) ? "bg-base-300" : ""}`} href={props.route}>
+            <div className="flex justify-start items-center w-full h-full">
                 {
                     props.icon &&
                     <FontAwesomeIcon icon={props.icon} className="w-5 h-5 mr-3" />
                 }
                 {props.displayName}
-            </Link>
-        </div>
+            </div>
+        </Link>
     )
 
 }
--- src/app/space/[spaceId]/(components)/SideBarItems.tsx
+++ src/app/space/[spaceId]/(components)/SideBarItems.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { faBox, faDisplay, faTag } from "@fortawesome/free-solid-svg-icons";
 import SideBarItem from "./SideBarItem";
 import { usePathname } from "next/navigation";
@@ -12,19 +10,17 @@ type Props = {
 
 const SideBarItems = (props: Props) => {
 
-    const pathname = usePathname();
-
     const generateURL = (page: string) => `/space/${props.spaceId}/${page}`;
 
     return (
         <div className="w-full overflow-y-auto flex flex-col items-center">
-            <SideBarItem displayName="Dashboard" icon={faDisplay} route={generateURL("dashboard")} activePath={pathname} />
+            <SideBarItem displayName="Dashboard" icon={faDisplay} route={generateURL("dashboard")} />
             <div className="divider">Assets</div>
-            <SideBarItem displayName="Assets" icon={faBox} route={generateURL("assets")} activePath={pathname} />
-            <SideBarItem displayName="Locations" icon={faBox} route={generateURL("locations")} activePath={pathname} />
-            <SideBarItem displayName="Tags" icon={faTag} route={generateURL("tags")} activePath={pathname} />
+            <SideBarItem displayName="Assets" icon={faBox} route={generateURL("assets")} />
+            <SideBarItem displayName="Locations" icon={faBox} route={generateURL("locations")} />
+            <SideBarItem displayName="Tags" icon={faTag} route={generateURL("tags")} />
             <div className="divider">Access Control</div>
-            <SideBarItem displayName="Users" icon={faTag} route={generateURL("users")} activePath={pathname} />
+            <SideBarItem displayName="Users" icon={faTag} route={generateURL("users")} />
         </div>
     );
 
